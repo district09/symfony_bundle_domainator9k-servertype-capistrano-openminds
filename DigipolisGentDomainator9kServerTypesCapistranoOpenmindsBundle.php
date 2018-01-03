6bfcaae4366e002c7c5858ea73a3a3e4467937bc
Pieter MAssoels <massoelspieter@gmail.com>
2018-01-03 13:14:27 +0100

    initial commit

---
+++ DigipolisGentDomainator9kServerTypesCapistranoOpenmindsBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace DigipolisGent\Domainator9k\ServerTypes\CapistranoOpenmindsBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class DigipolisGentDomainator9kServerTypesCapistranoOpenmindsBundle extends Bundle
+{
+}
