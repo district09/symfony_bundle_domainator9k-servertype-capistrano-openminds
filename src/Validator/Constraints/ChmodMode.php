abface9fe0eb06dd79ceb533bcb8dfe741a3e80e
Jelle Sebreghts <sebreghts.jelle@gmail.com>
2025-01-09 11:37:36 +0100

    Fix ChmodMode constraint

--- src/Validator/Constraints/ChmodMode.php
+++ src/Validator/Constraints/ChmodMode.php
@@ -7,15 +7,19 @@ use Symfony\Component\Validator\Constraint;
 #[\Attribute]
 class ChmodMode extends Constraint
 {
-    public $special = '0';
 
-    public $user;
+    public function __construct(
+        public $special = '0',
+        public $user = null,
+        public $group = null,
+        public $other = null,
+        public $message = '"{{ mode }}" is not a valid chmod mode.',
+        public $patternMessage = '"{{ mode }}" Is not a valid chmod mode or does not match the required pattern "{{ pattern }}".',
+        mixed $options = null,
+        ?array $groups = null,
+        mixed $payload = null
+    ) {
+        parent::__construct($options, $groups, $payload);
+    }
 
-    public $group;
-
-    public $other;
-
-    public $message = '"{{ mode }}" is not a valid chmod mode.';
-
-    public $patternMessage = '"{{ mode }}" Is not a valid chmod mode or does not match the required pattern "{{ pattern }}".';
 }
